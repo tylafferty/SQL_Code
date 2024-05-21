b58e01bd96466ca23579ffeb3efbb5247ea5b6f4
tylafferty <tylafferty@gmail.com>
2024-05-21 13:24:50 -0400

    Add files via upload

---
+++ Layoffs_2020to2023/Layoff_Data_Exploration.sql
@@ -0,0 +1,88 @@
+# Exploratory Data Analysis
+# Note we are not to worried about a specific goal, but rather exploring potential areas of interest within the data
+
+SELECT *
+FROM layoffs_staging2;
+
+#Date range for data
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2;
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+# Looking at companies that went under and sorting by funds raised
+SELECT*
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+# Looking at the total amount of employees laid off by each company
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+# Looking at total amount of layoffs based on industry
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+# Looking at the total amount of people layed off based on country
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+# Total layoffs based on year
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 1 DESC;
+
+# Total Layoffs per month 
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC;
+
+# Rolling sum of layoffs
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC
+)
+SELECT `MONTH`, total_off ,SUM(total_off) OVER(ORDER BY `MONTH`) as rolling_total
+FROM Rolling_Total;
+
+# Yearly layoffs for each company
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+# Ranking largest layoffs per years
+WITH Company_Year (company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+),
+Company_Year_Rank AS
+(SELECT *, 
+DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+FROM Company_Year
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM Company_Year_Rank
+WHERE Ranking <= 5;
+
+
+
+
